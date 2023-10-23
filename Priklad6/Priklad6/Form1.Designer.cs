17770c0851b233d6622ecf93a4843f2971fd9cf7
Antoninstolfa <97514759+Antoninstolfa@users.noreply.github.com>
2023-10-23 13:14:00 +0200

    Add files via upload

---
+++ Priklad6/Priklad6/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Priklad6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1190, 698);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Priklad6/Priklad6/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Priklad6
+{
+    public partial class Form1 : Form
+    {
+        TextBox textbox1;
+        FontDialog fontdialog1;
+        ColorDialog colordialog1;
+        NumericUpDown numericUpDown1;
+        NumericUpDown numericUpDown2;
+        Label label1;
+        Label label2;
+        Button Uloz;
+        Button Zrus;
+        Button popredi;
+        Button pozadi;
+        Button font;
+        Color fore = Color.Black;
+        Color back = Color.White;
+        Font pismo = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Form form2 = new Form();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textbox1 = new TextBox();
+            textbox1.Location = new Point(20, 10);
+            textbox1.Size = new Size(120,20);
+            textbox1.Text = "Text tlačítka";
+
+            fontdialog1 = new FontDialog();
+
+            colordialog1 = new ColorDialog();
+
+            numericUpDown1 = new NumericUpDown();
+            numericUpDown1.Location = new Point(20, 40);
+            numericUpDown1.Size = new Size(50, 20);
+            numericUpDown1.Minimum = 50;
+            numericUpDown1.Maximum = 1000;
+
+            numericUpDown2 = new NumericUpDown();
+            numericUpDown2.Location = new Point(20, 70);
+            numericUpDown2.Size = new Size(50, 20);
+            numericUpDown2.Minimum = 50;
+            numericUpDown2.Maximum = 1000;
+
+            label1 = new Label();
+            label1.Location = new Point(5, 40);
+            label1.Size = new Size(15, 20);
+            label1.Text = "x:";
+
+            label2 = new Label();
+            label2.Location = new Point(5, 70);
+            label2.Size = new Size(15,20);
+            label2.Text = "y:";
+
+            popredi = new Button();
+            popredi.Location = new Point(20, 100);
+            popredi.Size = new Size(70, 20);
+            popredi.Text = "Barva textu";
+            popredi.Click += popredi_Click;
+
+            pozadi = new Button();
+            pozadi.Location = new Point(100, 100);
+            pozadi.Size = new Size(70, 20);
+            pozadi.Text = "Barva pozadí";
+            pozadi.Click += pozadi_Click;
+
+            font = new Button();
+            font.Location = new Point(70,130);
+            font.Size = new Size(70,20);
+            font.Text = "Font";
+            font.Click += font_Click;
+
+            Uloz = new Button();
+            Uloz.Location = new Point(20, 160);
+            Uloz.Size = new Size(70, 20);
+            Uloz.Text = "Ulož";
+            Uloz.Click += Uloz_Click;
+
+            Zrus = new Button();
+            Zrus.Location = new Point(20, 190);
+            Zrus.Size = new Size(70, 20);
+            Zrus.Text = "Zrušit";
+            Zrus.Click += Zrus_Click;
+
+            form2.Controls.Add(textbox1);
+            form2.Controls.Add(numericUpDown1);
+            form2.Controls.Add(numericUpDown2);
+            form2.Controls.Add(label1);
+            form2.Controls.Add(label2);
+            form2.Controls.Add(popredi);
+            form2.Controls.Add(pozadi);
+            form2.Controls.Add(font);
+            form2.Controls.Add(Uloz);
+            form2.Controls.Add(Zrus);
+
+            if (form2.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+        }
+
+        private void popredi_Click(object sender, EventArgs e)
+        {
+            if(colordialog1.ShowDialog() == DialogResult.OK)
+            {
+                fore = colorDialog1.Color;
+            }
+        }
+
+        private void pozadi_Click(object sender, EventArgs e)
+        {
+
+            if (colordialog1.ShowDialog() == DialogResult.OK)
+            {
+                back = colorDialog1.Color;
+            }
+        }
+
+        private void font_Click(object obj, EventArgs e)
+        {
+            if(fontdialog1.ShowDialog() == DialogResult.OK)
+            {
+                pismo = fontdialog1.Font;
+            }
+        }
+
+        private void Uloz_Click(object sender, EventArgs e)
+        {
+            button1.Text = textbox1.Text;
+            button1.Size = new Size((int)numericUpDown1.Value, (int)numericUpDown2.Value);
+            button1.ForeColor = fore;
+            button1.BackColor = back;
+            button1.Font = pismo;
+        }
+        private void Zrus_Click(object sender, EventArgs e)
+        {
+            form2.Close();
+        }
+    }
+}
